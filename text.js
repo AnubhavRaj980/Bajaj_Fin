c94b834481366dd677928e28c6541f7d6d9fb9fa
Anubhav Raj <76939615+Anubhav980@users.noreply.github.com>
2023-09-19 17:19:00 +0530

    First message

---
+++ text.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const serverless = require("serverless-http");
+const bodyParser = require('body-parser');
+const router = express.Router();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Hey this is my API running 🥳')
+})
+
+app.post('/bfhl', (req, res) => {
+  const { data } = req.body;
+
+  if (!data || !Array.isArray(data)) {
+    return res.status(400).json({ error: 'Invalid input data' });
+  }
+
+  // Calculate the highest alphabet in the input array of alphabets
+  const alphabets = data.filter((item) => isNaN(item));
+  const highestAlphabet = alphabets.reduce((max, alphabet) => {
+    return alphabet > max ? alphabet : max;
+  }, '');
+
+  const response = {
+    status: 'Success',
+    user_id: 'anubhav_raj_24092002', 
+    college_email: 'anubhav.raj2020@vitbhopal.ac.in', 
+    college_roll: '20BCE10678', 
+    numbers: data.filter((item) => !isNaN(item)),
+    alphabets: alphabets,
+    highest_alphabet: highestAlphabet,
+  };
+
+  res.status(200).json(response);
+});
+
+app.get('/bfhl', (req, res) => {
+  res.status(200).json({ operation_code: 1 });
+});
+
+app.use('/.netlify/functions/test', router);
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+module.exports.handler = serverless(app);
